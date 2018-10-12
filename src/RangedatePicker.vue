de732a7b84979f111ca942d358c5d9dec90693b9
Sam <awwafe@gmail.com>
2018-10-12 12:51:19 +1100

    Add noselect to calendar

--- src/RangedatePicker.vue
+++ src/RangedatePicker.vue
@@ -120,6 +120,14 @@
   background: #fff;
   position: absolute;
   z-index: 9;
+
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+   -khtml-user-select: none; /* Konqueror HTML */
+     -moz-user-select: none; /* Firefox */
+      -ms-user-select: none; /* Internet Explorer/Edge */
+          user-select: none; /* Non-prefixed version, currently
+                                supported by Chrome and Opera */
 }
 
 .calendar-head h2 {
